d830f004afc7d691d4442f639afe313e91a60474
Sara Regibo <sara@ster.kuleuven.be>
2024-01-22 22:27:07 +0100

    Function to schedule command

--- src/solareclipseworkbench/utils.py
+++ src/solareclipseworkbench/utils.py
@@ -29,5 +29,42 @@ def schedule_commands(filename: str, scheduler: BackgroundScheduler, reference_m
 
     with open(filename, "r") as file:
         for cmd_str in file:
-            print(cmd_str)
-            interpret_cmd_str(scheduler, reference_moments, cmd_str)
+            schedule_command(scheduler, reference_moments, cmd_str)
+
+
+def schedule_command(scheduler: BackgroundScheduler, reference_moments, cmd_str: str):
+    """ Schedule the given command with the given scheduler and reference moments.
+
+    Args:
+        - scheduler: Background scheduler to use to schedule the command
+        - reference_moments: Dictionary with the reference moments of the solar eclipse, as datetime objects
+        - cmd_str: Command string
+    """
+
+    cmd_str_split = cmd_str.split(",")
+    func_name = cmd_str_split[0].lstrip()
+    ref_moment = cmd_str_split[1].lstrip()
+    sign = cmd_str_split[2].lstrip()    # + or -
+    hours, minutes, seconds = cmd_str_split[3].lstrip().split(":")   # mm:ss.ss
+    description = cmd_str_split[-1].lstrip()
+
+    logging.info(f"Scheduling {func_name} at {ref_moment}{sign}{cmd_str_split[3].lstrip()}")
+
+    args = cmd_str_split[4:-1]
+
+    func = COMMANDS[func_name]
+
+    # ref_moment = datetime.now()
+    reference_moment = reference_moments[ref_moment]
+    delta = timedelta(hours=float(hours), minutes=float(minutes), seconds=float(seconds))
+
+    if sign == "+":
+        execution_time = ref_moment + delta
+    else:
+        execution_time = ref_moment - delta
+
+    trigger = CronTrigger(year=execution_time.year, month=execution_time.month, day=execution_time.day,
+                          hour=execution_time.hour, minute=execution_time.minute,
+                          second=execution_time.second)
+
+    scheduler.add_job(func, trigger=trigger, args=args, name=description)
