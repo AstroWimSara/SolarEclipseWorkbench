4b9680540a990f2b2471b431719662f8e5e60e8a
Sara Regibo <sara@ster.kuleuven.be>
2024-01-22 22:09:49 +0100

    Function to schedule commands
    This functions uses the given `BackgroundScheduler` object to schedule commands related to taking pictures and voice prompts.  The timing of these commands is specified in a file.  A 2nd file holds the timing of the reference moments (C1,..., C4, and maximum eclipse).

--- src/solareclipseworkbench/utils.py
+++ src/solareclipseworkbench/utils.py
@@ -8,3 +8,22 @@ def start_scheduler():
     scheduler.start()
 
     return scheduler
+
+
+def schedule_commands(filename: str, scheduler: BackgroundScheduler, reference_moments):
+    """ Schedule commands as specified in the given file.
+
+    Args:
+        - filename: Name of the file in which the commands have been listed, scheduled relatively to the given
+                    reference moments
+        - scheduler: Background scheduler to use to schedule the commands
+        - reference_moments: Dictionary with the reference moments (1st - 4th contact and maximum eclipse), with
+                             respect to which the commands are scheduled
+
+    Returns: Scheduler that is used to schedule the commands.
+    """
+
+    with open(filename, "r") as file:
+        for cmd_str in file:
+            print(cmd_str)
+            interpret_cmd_str(scheduler, reference_moments, cmd_str)
