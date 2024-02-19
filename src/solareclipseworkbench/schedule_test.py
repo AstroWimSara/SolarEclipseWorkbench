7e202b77b8b07d703e93a1d9969e19782e986120
Wim De Meester <wim.demeester@kuleuven.be>
2024-02-19 21:05:51 +0100

    Add example for scheduling tasks

--- src/solareclipseworkbench/schedule_test.py
+++ src/solareclipseworkbench/schedule_test.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from astropy.time import Time
 from datetime import datetime, timedelta
 from solareclipseworkbench.reference_moments import calculate_reference_moments
@@ -8,25 +10,21 @@ import camera
 
 def main():
     eclipse_date = Time('2024-04-08')
-    # eclipse_date = Time('2024-10-02')
+
     timings, magnitude, eclipse_type = calculate_reference_moments(-104.63525, 24.01491, 1877.3, eclipse_date)
-    # timings, magnitude, eclipse_type = calculate_reference_moments(-75.18647, -47.29000, 1877.3, eclipse_date)
 
     # filename = "/Users/wim/GitHub/SolarEclipseWorkbench/config/scripts/voice_prompts.txt"
-    filename = "/Users/wim/GitHub/SolarEclipseWorkbench/config/scripts/test.txt"
+    filename = "/Users/wim/GitHub/SolarEclipseWorkbench/config/scripts/testEOS80D.txt"
 
-    # camera_names = camera.get_cameras()
-    # print(camera_names)
-    # cameras = {}
-    # for camera_name in camera_names:
-    #     cameras[camera_name[0]] = camera.get_camera(camera_name[0])
+    cameras = camera.get_camera_dict()
 
     # C1 in 2 minutes
     simulated_start = datetime.now(pytz.utc) + timedelta(minutes=2)
 
-    scheduler = observe_solar_eclipse(timings, filename, {}, simulated_start)
+    scheduler = observe_solar_eclipse(timings, filename, cameras, simulated_start)
 
-    print(scheduler.get_jobs())
+    while len(scheduler.get_jobs()) > 0:
+        sleep(5)
 
 
 if __name__ == "__main__":
